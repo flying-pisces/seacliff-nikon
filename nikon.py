9abce9a283c6941e1d63066e178e092cbca3e39d
Chuck Yin <chuckyin@fb.com>
2020-04-02 15:39:14 -0700

    make aperture shutter iso configurable as arg format

--- nikon.py
+++ nikon.py
@@ -1,44 +1,41 @@
 import sys
 import os
 import subprocess
-import datetime 
+import datetime
 
-def func_TakeNikonPicture(input_filename):
-    camera_command = 'C:\Program Files (x86)\digiCamControl\CameraControlCmd.exe'
-    camera_command_details = '/filename ./' + input_filename + ' /capture /iso 500 /shutter 1/30 /aperture 1.8'
-    print('camera details = ',camera_command_details)
-    full_command=camera_command + ' ' + camera_command_details
-    p = subprocess.Popen(full_command, stdout=subprocess.PIPE, universal_newlines=True, shell=False)
-    (output, err) = p.communicate()  
+class nikonError(Exception):
+    def __init__(self, value):
+        Exception.__init__(self)
+        self.value = value
 
-    #This makes the wait possible
-    p_status = p.wait(1)
-    # print(p.stdout.readline())
+    def __str__(self):
+        return repr(self.value)
 
-    #This will give you the output of the command being executed
-    print('Command output: ' + str(output))
-    print('Command err: ' + str(err))
+class nikon():
+    def __init__(self):
+        self.name = "nikon"
 
-    print('done')
+    def capture(self, filename = "test.png", iso = "500", shutter = "1/30", aperture = "1.8"):
+        camera_command = 'CameraControlCmd.exe'
+        camera_command_details = '/filename ./' + filename + ' /capture /iso '+ iso + ' /shutter ' + shutter + ' /aperture' + aperture
+        print('camera details = ',camera_command_details)
+        full_command=camera_command + ' ' + camera_command_details
+        p = subprocess.Popen(full_command, stdout=subprocess.PIPE, universal_newlines=True, shell=False)
+        (output, err) = p.communicate()
+
+        #This makes the wait possible
+#        p_status = p.wait(1)
+        # print(p.stdout.readline())
+
+        #This will give you the output of the command being executed
+        print('Command output: ' + str(output))
+        print('Command err: ' + str(err))
+
+        print('done')
 
 
-if(len(sys.argv) < 2):
-    rawimagename = 'test.jpg'
-else:   
-    # sys.argv[0] is the program name, sys.argv[1] is the first file, etc.
-    # need to shift this over
-    files = sys.argv[1:len(sys.argv)]
-    # Read the image
-    rawimagename = files[0]
-    if(os.path.isfile(rawimagename) is True):
-        print("File exists...not overwriting.")
-        sys.exit()
-
-# Store date/time for file uniqueness
-current_dt=datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-print("Current date time = " + current_dt)
-rawimagename=current_dt + '_' + rawimagename
-
-print('Name of raw image will be: ', rawimagename)
-
-func_TakeNikonPicture(rawimagename)
+if __name__ == "__main__":
+    rawimagename= "test.png"
+    print('Name of raw image will be: ', rawimagename)
+    the_camera = nikon()
+    the_camera.capture(rawimagename)
